f3f8621966d688ef8eda9369d0450318cc97be07
takaki-ishibashi <itakaki0@gmail.com>
2019-06-04 22:53:04 +0900

    Clean up ArrayStack

--- js/ArrayStack.js
+++ js/ArrayStack.js
@@ -1,8 +1,8 @@
 'use strict';
-var ArrayStack = function () {
-  this.n = 0;
+const ArrayStack = function () {
+  this.n = 0; // number of element
   this.a = [];
-  this.a.length = 5;
+  this.a.length = 5; // initial length of the list
   
   this.size = function () {
     return this.n;
@@ -14,37 +14,45 @@ var ArrayStack = function () {
   
   this.set = function (i, x) {
     if (0 <= i && i < (this.a.length - 1)) {
-      var y = this.a[i];
+      let y = this.a[i];
       this.a[i] = x;
       return y;
     }
   }
 
   this.resize = function () {
-    var b = [];
+    let b = [];
     b.length = this.n * 2;
-    for (var i=0; i<this.n; i++) {
+    for (let i = 0; i < this.n; i++) {
       b[i] = this.a[i];
     }
     this.a = b;
   }
 
   this.add = function (i, x) {
-    if ((this.n+1) >= this.a.length) this.resize();
-    for (var j=this.n; j>i; j--) {
-      this.a[j] = this.a[j-1];
+    if ((this.n + 1) >= this.a.length) this.resize();
+    for (let j = this.n; j > i; j--) { // 末尾に新規要素を追加して、１つ前の要素を後ろの要素にシフト
+      this.a[j] = this.a[j-1]; // resizeを無視するとO(n-i)
     }
     this.a[i] = x;
     this.n++;
   }
 
   this.remove = function(i) {
-    var x = this.a[i];
-    for (var j=i; j<(this.n-1); j++) {
-      this.a[j] = this.a[j+1];
+    let x = this.a[i];
+    for (let j = i; j < (this.n - 1); j++) { // １つ前の要素に後ろの要素をシフト
+      this.a[j] = this.a[j + 1]; // O(n-i)
     }
     this.n--
-    if (this.length >= (3*this.n)) resize();
+    if (this.a.length >= (3 * this.n)) this.resize();
     return x;
   }
 }
+
+let as = new ArrayStack();
+as.add(0,2)
+as.add(1,5)
+as.add(2,1)
+as.get(1)
+console.log(as.size())
+as.remove(1)
