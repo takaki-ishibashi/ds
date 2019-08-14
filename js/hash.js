79d848b7b2d70b2667a743ec13f1d1dd9aa257db
takaki-ishibashi <itakaki0@gmail.com>
2019-08-14 23:10:07 +0900

    Test for commit in Ubuntu

--- js/hash.js
+++ js/hash.js
@@ -11,7 +11,7 @@ function main(s) {
   var d = 3;
   var z = 123456789;
  h = (z * hc) >>> (w - d);
-// h = Math.trunc(((z * hc) % Math.pow(2, w)) / Math.pow(2,(w-d)));
+  // h = Math.trunc(((z * hc) % Math.pow(2, w)) / Math.pow(2,(w-d)));
   return h;
 }
 console.log(main('foo'));
